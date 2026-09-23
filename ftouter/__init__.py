b976d3cc66a5a21fa1acb47873f13a380b9a8c7c
Irfan-gitt <youcanbet5@gmail.com>
2026-09-23 11:15:56 +0530

    module fixed

--- ftouter/__init__.py
+++ ftouter/__init__.py
@@ -1,6 +1,13 @@
 from .providers import PROVIDER_INFO
-from . import chat_models, general_models, reasoning_models, tool_call, vision_models
+from . import chat_models, reasoning_models, tool_calling_models, general_models, vision_models
 from .router import check_available_models
 
-__all__ = ["PROVIDER_INFO", "chat_models", "reasoning_models", "tool_call",
-           "general_models", "vision_models", "check_available_models"]
+__all__ = [
+    "PROVIDER_INFO",
+    "chat_models",
+    "reasoning_models",
+    "tool_calling_models",
+    "general_models",
+    "vision_models",
+    "check_available_models",
+]
